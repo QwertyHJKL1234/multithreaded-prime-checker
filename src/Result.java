35a6610cfeff6c155da7706dd0a083109c0eeccd
Qwerty <QwertyHJKL11234@gmail.com>
2025-03-13 12:02:55 -0700

    Create Result.java

---
+++ src/Result.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+public class Result implements Serializable {
+    public long number;
+    public long factor1;
+    public long factor2;
+    public boolean isPrime;
+    public Result(long number, long factor1, boolean isPrime) {
+        this.number = number;
+        this.factor1 = factor1;
+        this.factor2 = number / factor1;
+        this.isPrime = isPrime;
+    }
+}
